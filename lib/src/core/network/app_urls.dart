63c8ad2bc75ad682c675ec68bc6daa356b0ffea9
Raks-Javac <rufaikudus2014@gmail.com>
2023-10-28 16:09:45 +0100

    ref: setting up dog repository

--- lib/src/core/network/app_urls.dart
+++ lib/src/core/network/app_urls.dart
@@ -3,7 +3,13 @@ class AppUrls {
 
 //Base Service Url
   static String baseUrl = "https://dog.ceo/api";
+  static String breedService = "/breed";
 
   //All Breeds
-  static String allBreedsUrl = "/breeds/list/all";
+  static const String allBreedsUrl = "/breeds/list/all";
+  static const String getRandomImageByBreedUrl = "/images/random";
+  static const String imageListByBreed = '/images';
+
+  static String randomImageByBreed = '/images/random';
+  static String randomImageBySubBreed = '/hound/afghan/images/random';
 }
--- lib/src/features/home/repository/dog_repository.dart
+++ lib/src/features/home/repository/dog_repository.dart
@@ -3,16 +3,57 @@ import 'package:paws/src/core/network/network_service.dart';
 
 abstract class AbstractDogRepository {
   Future<Map<String, dynamic>> getAllBreeds();
+  Future<Map<String, dynamic>> getRandomImageByBreed(String breedName);
+  Future<Map<String, dynamic>> getRandomImageBySubBreed(
+      String breedName, String subBreed);
+  Future<Map<String, dynamic>> imageListByBreed(String breedName);
+  Future<Map<String, dynamic>> imageListBySubBreed(
+      String breedName, String subBreedName);
 }
 
 class DogRepository implements AbstractDogRepository {
   final PawNetworkService _networkService;
   DogRepository(this._networkService);
 
+// Get all breeds
   @override
   Future<Map<String, dynamic>> getAllBreeds() {
     final getAllBreedsRequest =
         _networkService.getRequest(AppUrls.allBreedsUrl);
+
+    return getAllBreedsRequest;
+  }
+
+  @override
+  Future<Map<String, dynamic>> getRandomImageByBreed(String breedName) {
+    final getImageByBreedRequest = _networkService.getRequest(
+        "${AppUrls.breedService}/$breedName${AppUrls.getRandomImageByBreedUrl}");
+
+    return getImageByBreedRequest;
+  }
+
+  @override
+  Future<Map<String, dynamic>> imageListByBreed(String breedName) {
+    final getAllBreedsRequest = _networkService.getRequest(
+        "${AppUrls.breedService}/$breedName${AppUrls.imageListByBreed}");
+
+    return getAllBreedsRequest;
+  }
+
+  @override
+  Future<Map<String, dynamic>> getRandomImageBySubBreed(
+      String breedName, String subBreed) {
+    final getAllBreedsRequest = _networkService.getRequest(
+        "${AppUrls.breedService}/$breedName/$subBreed${AppUrls.getRandomImageByBreedUrl}");
+
+    return getAllBreedsRequest;
+  }
+
+  @override
+  Future<Map<String, dynamic>> imageListBySubBreed(
+      String breedName, String subBreedName) {
+    final getAllBreedsRequest = _networkService.getRequest(
+        "${AppUrls.breedService}/$breedName/$subBreedName/${AppUrls.imageListByBreed}");
 
     return getAllBreedsRequest;
   }
